e67ae8d5e0f00c87314682d234e2dacea181a093
DusVen44 <Venny44@yahoo.com>
2020-10-05 15:59:28 -0500

    more API edits

--- action.js
+++ action.js
@@ -52,13 +52,12 @@ function getYoutubeJsonData() {
     let searchInput = $("#artist-song").val();
     let properInput = searchInput.replace(" ", /%20/);
     let instrument = $("#instrument").val();
-    const api = YOUTUBE_API;
-    console.log(api);
+    const api = "AIzaSyCDSarObwOZUGCY0KZavF_EX6rrlpB_kXA";
     const baseURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&q=how%20to%20play%20' + properInput + '%20' + instrument + '&id.videoId&snippet.title&snippet.channelTitle&snippet.title&snippet.thumbnails.default.url&type=video&maxResults=2&key=' + api;
     fetch(baseURL)
     .then(response => response.json())
     .then(responseJson => displayYoutubeResults(responseJson))
-    .catch(err => alert("Reached YouTube Quota Limit - Site Administrator is requesting increase. Please try again later.", err));
+    .catch(err => alert("YouTube's API is acting up again. Site Administrator is dealing with it.", err));
 }
 
 //function to display the search results for YouTube
